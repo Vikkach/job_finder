77edbba76ba0948b95f2e7521588447e22fd7ea3
Vikkach <kach.vikt@gmail.com>
2018-04-30 18:54:49 +0300

    separate to methods

--- job_parser.py
+++ job_parser.py
@@ -1,44 +1,106 @@
-import os  
+import os
 import sqlite3
-from selenium import webdriver  
-from selenium.webdriver.common.keys import Keys  
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 FRONT_PAGE = "https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=QA"
 KEY = 'Python'
 
 
-chrome_options = Options()  
-chrome_options.add_argument("--headless")  
-
-driver = webdriver.Chrome(executable_path=os.path.abspath('c:\windows\system32\chromedriver'),   chrome_options=chrome_options)  
-driver.get(FRONT_PAGE)
-
-url_links = []
-job_list = driver.find_elements_by_class_name('vt')
-for job in job_list:
-    url_links.append(job.get_attribute('href'))
+def init():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(executable_path = os.path.abspath('c:\windows\system32\chromedriver'),
+                              chrome_options=chrome_options)
+    return driver
 
 
-for link in url_links:
-    driver.get(link)
-    job_list = []
-    job_description = driver.find_element_by_class_name('b-vacancy').text
-    if 'Python' in job_description:
-        company = driver.find_element_by_class_name('l-n').text
-        title = driver.find_element_by_class_name('g-h2').text
-        
-        
-        conn = sqlite3.connect('jobs.sqlite')
-        cur = conn.cursor()
+def get_list_of_all_vacancies(driver):
+    driver.get(FRONT_PAGE)
+    while True:
+        try:
+            wait = WebDriverWait(driver, 10)
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="vacancyListId"]/div')))
+            driver.find_element_by_link_text("Больше вакансий").click()
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="vacancyListId"]/div')))
+        except:
+            break
 
-        cur.execute('''CREATE TABLE IF NOT EXISTS Jobs
-                    (id INTEGER UNIQUE, company TEXT, title TEXT,
-                     url TEXT)''')
-        cur.execute('''INSERT INTO Jobs (company, title, url) VALUES (?, ?, ?)''', (company, title, link))
-        conn.commit()
-        print('------')
 
-#print(url_links)
+def jobs_db_init():
+    conn = sqlite3.connect('jobs.sqlite')
+    cur = conn.cursor()
+
+    cur.execute('''DROP TABLE IF EXISTS Jobs''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Jobs
+            (company TEXT, title TEXT,
+             url TEXT, address TEXT, address_url TEXT)''')
+    return conn
+
+
+def find_all_vacancies(driver):
+    url_links = []
+    job_list = driver.find_elements_by_class_name('vt')
+    for job in job_list:
+        url_links.append(job.get_attribute('href'))
+    return url_links
+
+
+def company_name(driver):
+    company = driver.find_element_by_class_name('l-n').text
+    company = company[:len(company) - 22]
+    return company
+
+
+def company_title(driver):
+    title = driver.find_element_by_class_name('g-h2').text
+    return title
+
+
+def company_detail_screen(driver, company):
+    address_info = []
+    driver.find_element_by_partial_link_text(company).click()
+    driver.get(driver.current_url + 'offices/')
+    try:
+        address = driver.find_elements_by_class_name('address')
+        address = address[0].text
+        address = address[:len(address) - 20]
+        address_info.append(address)
+        address_url = 'https://www.google.com.ua/maps/dir/Киев, ул. Драгоманова 40 з/Киев, ' + address
+        address_info.append(address_url)
+    except:
+        address_info = []
+        address_info.append('No address')
+        address_info.append('')
+    return address_info
+
+
+def info_about_vacancy(driver):
+    for link in find_all_vacancies(driver):
+        driver.get(link)
+        job_description = driver.find_element_by_class_name('b-vacancy').text
+        if KEY in job_description:
+            company = company_name(driver)
+            title = company_title(driver)
+            address_details = company_detail_screen(driver, company)
+            address = address_details[0]
+            address_url = address_details[1]
+
+            jobs_db.cursor().execute('''INSERT INTO Jobs (company, title, url, address, address_url) 
+                                    VALUES (?, ?, ?, ?, ?)''', (company, title, link, address, address_url))
+            jobs_db.commit()
+
+
+driver = init()
+jobs_db = jobs_db_init()
+get_list_of_all_vacancies(driver)
+info_about_vacancy(driver)
+driver.quit()
+
 
 
